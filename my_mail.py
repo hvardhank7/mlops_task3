8afa5aac14868a59f51ecb35043e779648795236
hvardhank7 <49608034+hvardhank7@users.noreply.github.com>
2020-05-26 14:09:26 +0530

    Update my_mail.py

--- my_mail.py
+++ my_mail.py
@@ -10,11 +10,11 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("hvardhank7@gmail.com", "hvk75837") 
+s.login("tarungarg20001@gmail.com", "Darpan786") 
 # message to be sent 
 message = "Hey Developer, you need to check your code once. Might be this have some error. "
 # sending the mail 
-s.sendmail("hvardhank7@gmail.com", "hvardhank7@gmail.com", message) 
+s.sendmail("tarungarg20001@gmail.com", "hvardhank7@gmail.com", message) 
 # terminating the session 
 s.quit()
 
